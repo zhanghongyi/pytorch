4cbdf18cae6dbe06d80525db3c8a283ac43cfee8
albanD <desmaison.alban@gmail.com>
2021-03-29 10:21:14 -0400

    Add gradcheck for forward AD by default and functional API
    ghstack-source-id: c451fce5304025494933debe0d7337f2beb39fbd
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/49099

--- test/test_sparse.py
+++ test/test_sparse.py
@@ -4,6 +4,9 @@ import torch
 # Sparse tests use double as the default dtype
 torch.set_default_dtype(torch.double)
 
+# TODO(alband) Remove this when this flag is not needed anymore
+torch._C._set_forward_AD_enabled(True)
+
 import itertools
 import functools
 import operator
@@ -290,7 +293,7 @@ class TestSparse(TestCase):
             def fn(x):
                 return x.to_dense()
             x.requires_grad_(True)
-            gradcheck(fn, (x,), check_sparse_nnz=True)
+            gradcheck(fn, (x,), check_sparse_nnz=True, check_forward=True)
 
         i = self.index_tensor([
             [0, 1, 2, 2],
@@ -416,7 +419,7 @@ class TestSparse(TestCase):
             def fn(x):
                 return x.to_dense()
             x.requires_grad_(True)
-            gradcheck(fn, (x,), check_sparse_nnz=True)
+            gradcheck(fn, (x,), check_sparse_nnz=True, check_forward=True)
 
         i = self.index_tensor([
             [0, 1, 2, 2],
@@ -1190,7 +1193,7 @@ class TestSparse(TestCase):
 
             def fn(S, D1, D2):
                 return torch.sparse.addmm(D1, S, D2)
-            gradcheck(fn, (S, D1, D2), check_sparse_nnz=True)
+            gradcheck(fn, (S, D1, D2), check_sparse_nnz=True, check_forward=True)
 
         test_shape(7, 8, 9, 20, False)
         test_shape(7, 8, 9, 20, True)
@@ -1209,7 +1212,7 @@ class TestSparse(TestCase):
 
             def fn(S, D):
                 return torch.sparse.mm(S, D)
-            gradcheck(fn, (S, D), check_sparse_nnz=True)
+            gradcheck(fn, (S, D), check_sparse_nnz=True, check_forward=True)
 
         test_shape(7, 8, 9, 20, False)
         test_shape(7, 8, 9, 20, True)
@@ -1370,7 +1373,7 @@ class TestSparse(TestCase):
                     if res.is_sparse:
                         res = res.to_dense()
                     return res
-                gradcheck(fn, (S,), check_sparse_nnz=True)
+                gradcheck(fn, (S,), check_sparse_nnz=True, check_forward=True)
 
             else:
                 S_sum = torch.sparse.sum(S, td)
@@ -1382,7 +1385,7 @@ class TestSparse(TestCase):
                     if res.is_sparse:
                         res = res.to_dense()
                     return res
-                gradcheck(fn, (S,), check_sparse_nnz=True)
+                gradcheck(fn, (S,), check_sparse_nnz=True, check_forward=True)
 
         nnz = 10
         sparse_dims = 2
