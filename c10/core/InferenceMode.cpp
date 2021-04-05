33c95158505ba44194c90a7931171a2207c00935
Ailing Zhang <ailzhang@fb.com>
2021-04-05 19:42:56 +0000

    Separate TLS for InferenceMode
    I tried to avoid creating new TLS, but InferenceMode::is_enabeld()
    is in perf critical path (TensorImpl constructor) so it seems
    worth adding one for it.
    This PR reduces one sources of instruction count increased by
    https://github.com/pytorch/pytorch/pull/55008.
    ```
     λ ~ python compare.py
    <torch.utils.benchmark.utils.valgrind_wrapper.timer_interface.FunctionCounts object at 0x7f59097ef310>
         100  0x0000000004854750
        -100  0x0000000004854760
       -4400  c10::impl::tls_is_dispatch_key_included(...)
    ```
    
    ghstack-source-id: 84dce14f19a073eaf70bc918466f0089b5578c73
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55238

--- c10/core/InferenceMode.cpp
+++ c10/core/InferenceMode.cpp
@@ -1,11 +1,36 @@
-
 #include <c10/core/InferenceMode.h>
+#include <stdexcept>
 
 namespace c10 {
+/// thread_local is a feature that is not enabled by Caffe2 mobile
+/// build (e.g. iOS). Therefore, we only provide `at::GradMode`
+/// when we are not in mobile build or when FEATURE_TORCH_MOBILE
+/// is on.
+#if !defined(C10_MOBILE) || defined(FEATURE_TORCH_MOBILE)
+thread_local bool InferenceMode_enabled = false;
+
+// Invariant:
+//   is_enabled() == !c10::impl::tls_is_dispatch_key_included(DispatchKey::InplaceOrView);
+// InferenceMode::is_enabled() is in perf critical path (TensorImpl constructor)
+// so it worths a separate TLS to skip the DispatchKeySet check.
+bool InferenceMode::is_enabled() {
+  return InferenceMode_enabled;
+}
+
+void InferenceMode::set_enabled(bool enabled) {
+  InferenceMode_enabled = enabled;
+}
+#else
 
 bool InferenceMode::is_enabled() {
-  // See Note [Expected TLS state in InferenceMode]
-  return !c10::impl::tls_is_dispatch_key_included(DispatchKey::InplaceOrView);
+  return false;
 }
+
+void InferenceMode::set_enabled(bool enabled) {
+  throw std::runtime_error("InferenceMode::set_enabled is not supported on mobile");
+}
+
+#endif
+
 
 } // namespace c10
